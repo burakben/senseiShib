b1110a0fe76a274c7ae8703ffe2f64c91bf5cb05
Pinkmann <korgov.vova@icloud.com>
2021-12-14 18:24:59 +0200

    redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header/Header';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Swap from './components/Swap';
 import Liquidity from './components/Liquidity';
 
@@ -9,6 +9,7 @@ function App() {
         <>
             <Header />
             <Switch>
+                <Redirect path="/" to="/swap" exact />
                 <Route path="/swap" exact>
                     <Swap />
                 </Route>
