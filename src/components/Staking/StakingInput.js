70dcfb721a7acb7ef60f9d3f5cdc0625544dd557
Pinkmann <korgov.vova@icloud.com>
2022-01-27 17:19:37 +0200

    fix

--- src/components/Staking/StakingInput.js
+++ src/components/Staking/StakingInput.js
@@ -1,15 +1,15 @@
 import NumberFormat from "react-number-format";
 
 export default function StakingInput({ value, setValue, buttonTitle, onClick }) {
-
+    
     function handleInputChange(e) {
-        setValue(e.target.value);
+        setValue(e.value);
     }
 
     return (
         <div className="input-wrapper input-wrapper--staking">
             <div className="input-wrapper__row">
-                <NumberFormat className="input input-wrapper__input" value={value} onChange={handleInputChange} placeholder="Enter value" thousandSeparator={true} allowLeadingZeros={false} allowNegative={false} />
+                <NumberFormat className="input input-wrapper__input" value={value} onValueChange={handleInputChange} placeholder="Enter value" thousandSeparator={true} allowLeadingZeros={false} allowNegative={false} />
                 <button className="input-wrapper__max">MAX</button>
             </div>
             <button className="input-wrapper__button" onClick={onClick}>{buttonTitle}</button>
